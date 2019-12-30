8df388fca647768b9860d68a6e8574fe71023c40
Bia5310 <Bia5310@yandex.ru>
2019-12-30 06:31:26 +0300

    Добавьте файлы проекта.

---
+++ SpectrometrAV/CustomSlider.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SpectrometrAV
+{
+    public class CustomSlider : UserControl
+    {
+        public CustomSlider()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
